7def966be0b4d6183fee8dfa23d2695d459422d0
gokul <asdf1234@>
2021-07-12 20:34:23 +0530

    fixed issues

--- src/PostList/PostList.js
+++ src/PostList/PostList.js
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Card } from "@material-ui/core";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { CSVLink } from 'react-csv'
 import Paper from '@material-ui/core/Paper';
@@ -11,21 +11,18 @@ function PostList(props) {
             color: theme.palette.common.white,
         },
         body: {
-
-
             fontSize: 14,
         },
     }))(TableCell);
 
     const StyledTableRow = withStyles((theme) => ({
-
         root: {
             '&:nth-of-type(odd)': {
                 backgroundColor: theme.palette.action.hover,
-            },
-            'width': 10
+            }
         },
     }))(TableRow);
+
     const jsonTocsv = (json3) => {
         var json = json3
 
@@ -43,7 +40,7 @@ function PostList(props) {
 
     const useStyles = makeStyles({
         root: {
-            minWidth: 200,
+            minWidth: 1200,
 
             margin: 100
         }
@@ -59,12 +56,9 @@ function PostList(props) {
             } > Download CSV < /CSVLink>}   <
             TableContainer component = { Paper } >
             <
-            Table className = { classes.table } >
-            <
-            TableHead >
-            <
-            TableRow >
-            <
+            Table className = { classes.table } > <
+            TableHead > <
+            TableRow > <
             StyledTableCell align = "right" > IndicatorName < /StyledTableCell><
             StyledTableCell align = "right" > Jan19(g) < /StyledTableCell><
             StyledTableCell align = "right" > Feb19(g) < /StyledTableCell><
@@ -89,94 +83,52 @@ function PostList(props) {
             StyledTableCell align = "right" > Sep20(g) < /StyledTableCell><
             StyledTableCell align = "right" > Oct20(g) < /StyledTableCell><
             StyledTableCell align = "right" > Nov20(g) < /StyledTableCell><
-            StyledTableCell align = "right" > Dec20(g) < /StyledTableCell>
-
-            <
-            /
-            TableRow > <
+            StyledTableCell align = "right" > Dec20(g) < /StyledTableCell></TableRow > <
             /TableHead><
             TableBody > {
-                props.postdata.map((row) => ( <
-
-                    StyledTableRow key = { row["IndicatorName"] } >
-
-                    <
+                props.postdata.map((row) => ( < StyledTableRow key = { row["IndicatorName"] } > <
                     StyledTableCell align = "right" > {
-                        row["IndicatorName"]
+                        row["Indicator Name"]
                     } < /StyledTableCell><
                     StyledTableCell align = "right" > {
                         row["Jan-19"]
-                    } < /StyledTableCell>
-
-                    <
+                    } < /StyledTableCell><
                     StyledTableCell align = "right" > {
                         row["Feb-19"]
-                    } < /StyledTableCell>
-
-                    <
+                    } < /StyledTableCell><
                     StyledTableCell align = "right" > {
                         row["Mar-19"]
-                    } < /StyledTableCell>
-
-                    <
-                    StyledTableCell align = "right" > { row["Apr-19"] } < /StyledTableCell>
-
-                    <
-                    StyledTableCell align = "right" > { row["May-19"] } < /StyledTableCell>
-
-                    <
+                    } < /StyledTableCell> <
+                    StyledTableCell align = "right" > { row["Apr-19"] } < /StyledTableCell><
+                    StyledTableCell align = "right" > { row["May-19"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Jun-19"] } < /StyledTableCell><
-                    StyledTableCell align = "right" > { row["Jul-19"] } < /StyledTableCell>
-
-                    <
+                    StyledTableCell align = "right" > { row["Jul-19"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Aug-19"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Sep-19"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Oct-19"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Nov-19"] } < /StyledTableCell><
-                    StyledTableCell align = "right" > { row["Dec-19"] } < /StyledTableCell>
-
-
-                    <
+                    StyledTableCell align = "right" > { row["Dec-19"] } < /StyledTableCell><
                     StyledTableCell align = "right" > {
                         row["Jan-20"]
-                    } < /StyledTableCell>
-
-                    <
+                    } < /StyledTableCell><
                     StyledTableCell align = "right" > {
                         row["Feb-20"]
-                    } < /StyledTableCell>
-
-                    <
+                    } < /StyledTableCell><
                     StyledTableCell align = "right" > {
                         row["Mar-20"]
-                    } < /StyledTableCell>
-
-                    <
-                    StyledTableCell align = "right" > { row["Apr-20"] } < /StyledTableCell>
-
-                    <
-                    StyledTableCell align = "right" > { row["May-20"] } < /StyledTableCell>
-
-                    <
+                    } < /StyledTableCell><
+                    StyledTableCell align = "right" > { row["Apr-20"] } < /StyledTableCell><
+                    StyledTableCell align = "right" > { row["May-20"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Jun-20"] } < /StyledTableCell><
-                    StyledTableCell align = "right" > { row["Jul-20"] } < /StyledTableCell>
-
-                    <
+                    StyledTableCell align = "right" > { row["Jul-20"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Aug-20"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Sep-20"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Oct-20"] } < /StyledTableCell><
                     StyledTableCell align = "right" > { row["Nov-20"] } < /StyledTableCell><
-                    StyledTableCell align = "right" > { row["Dec-20"] } < /StyledTableCell>
-
-                    <
-                    /
-                    StyledTableRow >
+                    StyledTableCell align = "right" > { row["Dec-20"] } < /StyledTableCell>< /StyledTableRow >
                 ))
             } <
-            /TableBody></
-            Table > <
-            /TableContainer></
-            div >
+            /TableBody></Table > < /TableContainer></div >
         )
 
     }
